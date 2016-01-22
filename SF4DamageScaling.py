8e71354c725e04ac8aa653e962845034e83fd63e
mmiller10196 <matt.miller10196@gmail.com>
2016-01-22 16:37:02 -0500

    Update SF4DamageScaling.py
    Added comments

--- SF4DamageScaling.py
+++ SF4DamageScaling.py
@@ -1,5 +1,10 @@
+# This program calculates the damage or stun of a combo in the video game
+# Ultra Street Fighter 4 based on user inputs for combo length and
+# damage or stun values.
+
 import math
 
+# the main function; adds up scaled values for damage/stun
 def main():
     val_list = []
     combo_length = checkIfInt()
@@ -8,19 +13,15 @@ def main():
         try:
             val_list += [int(val)]
         except ValueError:
+            # takes the user to the start of the program if
+            # they do not enter a number
             print "Invalid input. Please enter a number next time."
             print "Restarting:"
             main()
-    comboDamage = 0
-    for i in range(len(val_list)):
-        if (i == 0 or i == 1):
-            comboDamage += val_list[i]
-        elif (i >=2 and i <= 8):
-            comboDamage += math.ceil(val_list[i]- (i * 0.1 * val_list[i]))
-        else:
-            comboDamage += math.ceil(0.1 * val_list[i])
-    print "Your combo does", int(comboDamage), "damage/stun!"
+    # prints the damage or stun of the given combo
+    print "Your combo does", (scale(val_list)), "damage/stun!"
 
+# checks if the user actually enters a number
 def checkIfInt():
     valid = False
     while (not(valid)):
@@ -32,4 +33,17 @@ def checkIfInt():
             print "Please enter a number."
     return combo_length_int
 
+# performs the scaled summation of moves' damage/stun values
+def scale(vals):
+    scaled_sum = 0
+    for i in range(len(vals)):
+        if (i == 0 or i == 1):
+            scaled_sum += vals[i]
+        elif (i >=2 and i <= 8):
+            scaled_sum += math.ceil(vals[i]- (i * 0.1 * vals[i]))
+        else:
+            scaled_sum += math.ceil(0.1 * val_list[i])
+    return int(scaled_sum)
+
 main()
+
